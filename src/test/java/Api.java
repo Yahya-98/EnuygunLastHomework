b0cb94f8a2dd30bf748043e584b61f39a71e611a
Yahya DEMİR <yahyademir822@gmail.com>
2022-08-21 22:43:20 +0300

    report will add

--- src/test/java/Api.java
+++ src/test/java/Api.java
@@ -1,18 +1,22 @@
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+
 
 public class Api {
     Response response;
+    JsonPath jp;
 
    public Api(){
 
        baseURI = "https://petstore.swagger.io/v2/pet/";
    }
 
-    public int getRequest(String requestUrl){
+    public Response getRequest(String requestUrl){
         response = given()
                 .header("content-type","application/json")
                 .when()
@@ -22,25 +26,69 @@ public class Api {
                 .and()
                 .extract()
                 .response();
-        return response.statusCode();
+        return response;
     }
 
-    public void apiGetidStatusCheck(int id){
+    public boolean apiGetidStatusCheck(String status,int index){
+
+       jp = getRequest("findByStatus?status=" + status).jsonPath();
+       String id = String.valueOf(jp.getList("id").get(index));
+       String name = String.valueOf(jp.getList("name").get(index));
+       String oldStatus = String.valueOf(jp.getList("status").get(index));
+
+       jp = getRequest(id).jsonPath();
+       String petid = jp.get("id").toString();
+       String petname = jp.get("name");
+       String petstatus = jp.get("status");
+
+       // if petdata same return true else return false
+        return id.equals(petid) && name.equals(petname) && oldStatus.equals(petstatus);
+
+    }
+
+    public int apiPostRequest() throws UnirestException {
+       //request availeble pet
+        jp = getRequest("findByStatus?status=available").jsonPath();
+        // find second available pet's id
+        String id = String.valueOf(jp.getList("id").get(1));
+        String petNewName = "newpetname";
+        String petNewstatus = "pending";
+        // post id, newname and new status
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response = Unirest.post(baseURI + id)
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .field("name", petNewName)
+                .field("status", petNewstatus)
+                .asString();
+
+        // get pet id and check the update
+        jp = getRequest(id).jsonPath();
+        String petname = jp.get("name");
+        String petstatus = jp.get("status");
+        if(petname.equals(petNewName) && petstatus.equals(petNewstatus)){
+            return  response.getStatus();
+        }else{
+            return 404;
+        }
+
+    }
+    public Response  apiDeleteRequest(String id){
 
         response = given()
                 .header("content-type","application/json")
                 .when()
-                .get(String.valueOf(id))
+                .delete(id)
                 .then()
                 .statusCode(200)
                 .and()
                 .extract()
                 .response();
 
-        JsonPath jp = response.jsonPath();
-        String i = String.valueOf(jp.getList("id").get(2));
+        return response;
+
 
     }
+
 
 
 }
--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,4 +1,6 @@
 
+import com.mashape.unirest.http.exceptions.UnirestException;
+import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -6,27 +8,41 @@ import org.testng.annotations.Test;
 
 public class ApiTest {
     Api api = new Api();
+    JsonPath jp;
     @Test
     public void statusAvailableTest()  {
 
-        Assert.assertEquals(api.getRequest("findByStatus?status=available"),200);
+        Assert.assertEquals(api.getRequest("findByStatus?status=available").statusCode(),200);
+
     }
 
     @Test
     public void statusPendingTest(){
 
-        Assert.assertEquals(api.getRequest("findByStatus?status=pending"),200);
+        Assert.assertEquals(api.getRequest("findByStatus?status=pending").statusCode(),200);
     }
 
     @Test
     public void statusSoldTest(){
 
-        Assert.assertEquals(api.getRequest("findByStatus?status=sold"),200);
+        Assert.assertEquals(api.getRequest("findByStatus?status=sold").statusCode(),200);
     }
     @Test
     public void Check(){
-        api.apiGetidStatusCheck(12);
+        Assert.assertEquals(api.apiGetidStatusCheck("available",2),true);
     }
 
+    @Test
+    public void postPetIdTest() throws UnirestException {
+        Assert.assertEquals(api.apiPostRequest(),200);
+    }
+    @Test
+    public void deletePetTest(){
+
+        jp = api.getRequest("findByStatus?status=available").jsonPath();
+        String id = String.valueOf(jp.getList("id").get(5));
+
+        Assert.assertEquals(api.apiDeleteRequest(id).statusCode(),200);
+    }
 
 }
